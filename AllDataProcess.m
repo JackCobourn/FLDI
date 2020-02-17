41eededb4d669839f9c028040d62564124936e0b
Jack Cobourn <Jack@cobourn.com>
2020-02-17 08:47:11 -0600

    Update AllDataProcess.m

--- AllDataProcess.m
+++ AllDataProcess.m
@@ -10,6 +10,7 @@ for i = 1:length(Matfiles)
 Matfiles(i).Fullname = [Matfiles(i).folder,'\',Matfiles(i).name];
 M(i).Matfiles = matfile(Matfiles(i).Fullname);
 end
+EarlyRunNumbers = [1 2 3 4 5 7 6 8] ;
 
 %% Create Waterfall Plot
 ham=100; %number of divisions for hamming windows
@@ -17,6 +18,12 @@ start = 0.1; %[s] trim window start %will eventuall calc dynammically from p dat
 stop=0.2; %[s] trim window stop
 
 for i = 1:length(Matfiles)
+    if any(i==1:8) %Add run number to early runs
+        RunNumber(i,1) = EarlyRunNumbers(i);
+    else
+        RunNumber(i,1) = M(i).Matfiles.CampainRunNum;
+    end
+    Numpoints =     
     Y(i,1) = M(i).Matfiles.y_dist;
     VOLT(i,:) = [M(i).Matfiles.vmax,M(i).Matfiles.vmin];
     FS(i,1) = M(i).Matfiles.Fs;
@@ -27,7 +34,7 @@ for i = 1:length(Matfiles)
 %         CHA_TRIM{i,1} = CHA_TRIM{i,1}.*1000-500;
 %     end
 end
-
+[~,ordercode] = sort(RunNumber);%reordercode
 % figure out how to deal with noise not recorded for 8A(i) = is(who(M(i).Matfiles,'chA_noise'))
 % calc spectra
 
@@ -48,6 +55,22 @@ ax1.XScale = 'log';
 ax1.ZScale = 'log';
 %ax1.View = [-37.5 30];
 ax1.View = [0 0];
+
+%% Create waterfall scatter plot
+fig3 = figure(3);
+for i = 1:length(Matfiles)
+    h3(i) = scatter(f{i,1},Y(i,1).*ones(size(f{i,1})),15,PSDa{i,1}','filled');
+    hold on
+end
+ax3 = fig3.Children;
+ax3.XScale = 'log';
+xlim([10e3 inf])
+colormap colorcube
+cb3 = colorbar();
+cb3.Ruler.Scale = 'log';
+cb3.Ruler.MinorTick = 'on';
+
+
 %% Create BL prof
 dx2 = 2.6e-3; %need to calc dynamically 
 for i = 1:length(Matfiles)
@@ -73,9 +96,7 @@ Uc(i,1) = (dx2)/DT(i,1);
 % plot(lag,cross_cor)
 end
 fig2 = figure(2);
-for i = 1:length(Matfiles)
-    h2(i) = plot(Uc(i,1),25.4*Y(i,1),'k*','LineWidth',1.5)
-    hold on
-end
+h2 = scatter(Uc(:,1),25.4*Y(:,1),'*');
+hold on
 xlabel('Velocity (m/s)')
 ylabel('Height above floor (mm)')
