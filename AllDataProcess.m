a1005c590d8429783938b5987ecce49713ae58b6
Jack Cobourn <Jack@cobourn.com>
2020-02-27 11:51:12 -0600

    Update all data process

--- AllDataProcess.m
+++ AllDataProcess.m
@@ -1,6 +1,6 @@
 %% Find Matfiles
 close all
-cd('D:\FLDI_UMD\Data\BL Profile Sweep')
+cd('D:\FLDI_UMD\Data')
 Matfiles = dir('**/*.mat')';
 cd('C:\Users\jcobourn\Documents\GitHub\Focused_Laser_Dif_interf')
 %cd('D:\Jack\Documents\GitHub\Focused_Laser_Dif_interf')
@@ -8,86 +8,112 @@ cd('C:\Users\jcobourn\Documents\GitHub\Focused_Laser_Dif_interf')
 %% Load Matfiles
 %using matfiles() fuction, so they don't load totally into %memory
 
-for i = 1:length(Matfiles)
-Matfiles(i).Fullname = [Matfiles(i).folder,'\',Matfiles(i).name];
-M(i).Matfiles = matfile(Matfiles(i).Fullname);
+for ii = 1:length(Matfiles)
+Matfiles(ii).Fullname = [Matfiles(ii).folder,'\',Matfiles(ii).name];
+M(ii).Matfiles = matfile(Matfiles(ii).Fullname);
 end
+%add CampainRunNum to initial runs Works out to this pattern asedon date
+%and naming
 EarlyRunNumbers = [1 2 3 4 5 7 6 8] ;
-
-%% Create Waterfall Plot
-ham=100; %number of divisions for hamming windows
-start = 0.1; %[s] trim window start %will eventuall calc dynammically from p data
-stop=0.2; %[s] trim window stop
-
-for i = 1:length(Matfiles)
-    if any(i==1:8) %Add run number to early runs
-        RunNumber(i,1) = EarlyRunNumbers(i);
-    else
-        RunNumber(i,1) = M(i).Matfiles.CampainRunNum;
-    end
-    %Numpoints =     
-    Y(i,1) = M(i).Matfiles.y_dist;
-    VOLT(i,:) = [M(i).Matfiles.vmax,M(i).Matfiles.vmin];
-    FS(i,1) = M(i).Matfiles.Fs;
-    %CHA{i,1} = M(i).Matfiles.chA_run; lets avoid loading the whole thing
-    CHA_TRIM{i,1} = M(i).Matfiles.chA_run(fix((FS(i).*start)):fix((FS(i).*stop)-1),1);        %CHA{i}(fix((FS(i).*start)):fix((FS(i).*stop)-1));
-    CHB_TRIM{i,1} = M(i).Matfiles.chB_run(fix((FS(i).*start)):fix((FS(i).*stop)-1),1);
-    BIT(i,1) = M(i).Matfiles.bitRes;
-    if BIT(i) == 8
-        CHA_TRIM{i,1} = CHA_TRIM{i,1}.*1000; %convert the teleydyne scope results to Volts
-        CHB_TRIM{i,1} = CHB_TRIM{i,1}.*1000; %note offset doesn't matter due to mean subtracted pwelch
-    end
-end
-[~,ordercode] = sort(RunNumber);%reordercode
-% figure out how to deal with noise not recorded for 8A(i) = is(who(M(i).Matfiles,'chA_noise'))
-% calc spectra
-
-for i = 1:length(Matfiles)
-    [PSDa{i,1}, f{i,1}] = pwelch(CHA_TRIM{i,1}-mean(CHA_TRIM{i,1}),ceil(length(CHA_TRIM{i,1})/ham),[],[],fix(FS(i,1)));
-    %calc PSD area
-    PSDArea(i,1) = trapz(PSDa{i,1}).*mean(diff(f{i,1}));
-    PSDNorm{i,1} = PSDa{i,1}./PSDArea(i,1); %normalize to area of 1
-        
-%         if BIT(i) == 15 %account for fs difference
-%             PSDNorm{i,1} = PSDNorm{i,1}*(20/12.5);
-%         elseif BIT(i) == 8
-%             PSDNorm{i,1} = PSDNorm{i,1}*(12.5/20);
-%         end
-        NormResults(i,1) = trapz(PSDNorm{i,1}(1:find(f{i,1}>=f{1,1}(end),1)));
-end
-
-
-%loglog(f,PSDa,'linewidth',2)
-fig1 = figure(1);
-
-for i = 1:length(Matfiles)
-    h(i) = waterfall(f{i,1},Y(i,1),PSDNorm{i,1}');
-    hold on
-end
-ax1 = fig1.Children;
-ax1.XScale = 'log';
-%ax.YScale = 'log'
-ax1.ZScale = 'log';
-%ax1.View = [-37.5 30];
-ax1.View = [0 0];
-
-%% Create waterfall scatter plot
-% fig3 = figure(3);
-% for i = 1:length(Matfiles)
-%     h3(i) = scatter(f{i,1},Y(i,1).*ones(size(f{i,1})),15,PSDa{i,1}','filled');
-%     hold on
+% for ii = 1:8
+%     M(ii).Matfiles.CampainRunNum = EarlyRunNumbers(ii);
 % end
-% ax3 = fig3.Children;
-% ax3.XScale = 'log';
-% xlim([10e3 inf])
-% colormap colorcube
-% cb3 = colorbar();
-% cb3.Ruler.Scale = 'log';
-% cb3.Ruler.MinorTick = 'on';
+emptyIndex = [];
+%M.Matfiles = M(~find(M(1:31).Matfiles.num_diaphrams ~= 3).Matfiles
+%exclude runs not That have erronious data
+for ii = 1:length(Matfiles)
+if ii>8  %eliminate by particular campain numbers, below 8 don't have this number;
+    if M(ii).Matfiles.CampainRunNum == 27 || M(ii).Matfiles.CampainRunNum == 21 || M(ii).Matfiles.CampainRunNum == 12
+    %M(ii).Matfiles = [];
+    emptyIndex(ii) = 1;
+    else
+        emptyIndex(ii) = 0;
+    end
+else
+    emptyIndex(ii) = 0;
+end
+end
+M = M(~emptyIndex);
+clear emptyIndex
+for ii = 1:length(M)
+    if any(ii==1:8) %Add run number to early runs
+        M(ii).RunNumber = EarlyRunNumbers(ii);
+    else
+        M(ii).RunNumber = M(ii).Matfiles.CampainRunNum;
+    end
+end
+%Put mat files in run order in the script
+[~,ordercode] = sort([M.RunNumber]);%reordercode
+M = M(ordercode);
+clear ordercode EarlyRunNumbers
 
+%% move runs accosciated with different Re to a different matfile struct
+%gets rid of runs at different Re
+% for ii = 1:length(M)
+% if M(ii).Matfiles.num_diaphrams ~= 3
+%     WrongReIndex(ii) = true;
+% else
+%     WrongReIndex(ii) = false;
+% end
+% end
+% Re = M(WrongReIndex);
+% M = M(~WrongReIndex);
+% clear WrongReIndex
+
+%% Create Usable Cell Arrays
+start = readmatrix('D:\FLDI_UMD\DFLDI_UMD_UTSI_Campaign_M4_BL.xlsx','Sheet','Data','Range','K3:K32'); %[s] trim window start %will eventuall calc dynammically from p data
+stop = readmatrix('D:\FLDI_UMD\DFLDI_UMD_UTSI_Campaign_M4_BL.xlsx','Sheet','Data','Range','L3:L32'); %[s] trim window stop
+RunP0 = readmatrix('D:\FLDI_UMD\DFLDI_UMD_UTSI_Campaign_M4_BL.xlsx','Sheet','Data','Range','J3:J32');
+
+for ii = 1:length(M)
+    Details{ii,1} = whos(M(ii).Matfiles);     
+    Y(ii,1) = M(ii).Matfiles.y_dist;
+    VOLT(ii,:) = [M(ii).Matfiles.vmax(1,1),M(ii).Matfiles.vmin(1,1),M(ii).Matfiles.vmax(1,2),M(ii).Matfiles.vmin(1,2)];
+    FS(ii,1) = M(ii).Matfiles.Fs;
+    BIT(ii,1) = M(ii).Matfiles.bitRes;
+    
+    CHA_TRIM{ii,1} = M(ii).Matfiles.chA_run(fix((FS(ii).*start(ii))):fix((FS(ii).*stop(ii))-1),1);        %CHA{ii}(fix((FS(ii).*start)):fix((FS(ii).*stop)-1));
+    CHB_TRIM{ii,1} = M(ii).Matfiles.chB_run(fix((FS(ii).*start(ii))):fix((FS(ii).*stop(ii))-1),1);
+    if BIT(ii) == 8
+        CHA_TRIM{ii,1} = CHA_TRIM{ii,1}.*1000; %convert the teleydyne scope results to Volts
+        CHB_TRIM{ii,1} = CHB_TRIM{ii,1}.*1000; %note offset doesn't matter due to mean subtracted pwelch
+    end
+     
+end
+
+%% Calculate Re
+Mach = 4;
+gamma = 1.4;
+R = 287.058;
+[~, Tratio, Pratio, ~, ~] = flowisentropic(gamma, Mach, 'mach');
+P = RunP0.*Pratio.*6894.76; %convert psi to pa
+T = 300*Tratio;
+rho = P./(R.*T);
+a = sqrt(gamma*R*T);
+U0 = Mach*a;
+mu = 0.00001458*(T)^1.5/(T+110.4); %sotherlans law
+Rex = rho.*U0./mu;
+clear Mach gamma R Tratio Pratio P T rho a mu
+
+%% Spectral Analysis
+% 
+% for ii = 1:length(M)
+%     [PSDa{ii,1}, f{ii,1}] = pwelch(CHA_TRIM{ii,1}-mean(CHA_TRIM{ii,1}),ceil(length(CHA_TRIM{ii,1})/ham),[],[],fix(FS(ii,1)));
+%     %calc PSD area
+%     PSDArea(ii,1) = trapz(PSDa{ii,1}).*mean(diff(f{ii,1}));
+%     PSDNorm{ii,1} = PSDa{ii,1}./PSDArea(ii,1); %normalize to area of 1
+%         
+% %         if BIT(ii) == 15 %account for fs difference
+% %             PSDNorm{ii,1} = PSDNorm{ii,1}*(20/12.5);
+% %         elseif BIT(ii) == 8
+% %             PSDNorm{ii,1} = PSDNorm{ii,1}*(12.5/20);
+% %         end
+%         NormResults(ii,1) = trapz(PSDNorm{ii,1}(1:find(f{ii,1}>=f{1,1}(end),1)));
+% end
+% 
 %% Create IMageSc waterfall plot
 [~,Index] = sort(Y(:,1));
-for ii = 1:length(Matfiles)
+for ii = 1:length(M)
 PSDedit{ii,1} = PSDNorm{ii,1}(1:length(PSDa{1,1}))';
 end
 %PSDedit(:) = PSDa(:)(1:length(PSDa{1,1}))'; is there a way to do above in
@@ -99,7 +125,7 @@ imagesc('CData',log10(Colors(Index,:)))
 ax3 = fig3.Children;
 %xlim([1000,6000] )
 xlim([0,inf])
-ylim([0.5,length(Matfiles)+0.5])
+ylim([0.5,length(M)+0.5])
 colormap jet
  cb3 = colorbar();
  tickvals = cb3.TickLabels;
@@ -111,37 +137,74 @@ colormap jet
      text(100,ii,sprintf('y = %.2f, %d bit',Y(Index(ii)),BIT(Index(ii))))
  end
  
+%% Create Waterfall Plot
+% %loglog(f,PSDa,'linewidth',2)
+% fig1 = figure(1);
+% 
+% for ii = 1:length(M)
+%     h(ii) = waterfall(f{ii,1},Y(ii,1),PSDNorm{ii,1}');
+%     hold on
+% end
+% ax1 = fig1.Children;
+% ax1.XScale = 'log';
+% %ax.YScale = 'log'
+% ax1.ZScale = 'log';
+% %ax1.View = [-37.5 30];
+% ax1.View = [0 0];
 
-
-
-
+%% Create waterfall scatter plot
+% fig3 = figure(3);
+% for ii = 1:length(Matfiles)
+%     h3(ii) = scatter(f{ii,1},Y(ii,1).*ones(size(f{ii,1})),15,PSDa{ii,1}','filled');
+%     hold on
+% end
+% ax3 = fig3.Children;
+% ax3.XScale = 'log';
+% xlim([10e3 inf])
+% colormap colorcube
+% cb3 = colorbar();
+% cb3.Ruler.Scale = 'log';
+% cb3.Ruler.MinorTick = 'on';
 
 %% Create BL prof
-dx2 = 2.6e-3; %need to calc dynamically 
-for i = 1:length(Matfiles)
-[cross_cor{i,1},lag{i,1}] = xcorr(CHA_TRIM{i,1}-mean(CHA_TRIM{i,1}),-(CHB_TRIM{i,1}-mean(CHB_TRIM{i,1})), 'coeff');
+dx2_opts = readmatrix('D:\FLDI_UMD\DFLDI_UMD_UTSI_Campaign_M4_BL.xlsx','Sheet','Beam Pics','Range','K2:K10'); %need to calc dynamically 
+BeamPicRel = readmatrix('D:\FLDI_UMD\DFLDI_UMD_UTSI_Campaign_M4_BL.xlsx','Sheet','Data','Range','G3:G32');
+dx2 = dx2_opts(BeamPicRel);
+
+for ii = 1:length(M)
+[cross_cor{ii,1},lag{ii,1}] = xcorr(CHA_TRIM{ii,1}-mean(CHA_TRIM{ii,1}),-(CHB_TRIM{ii,1}-mean(CHB_TRIM{ii,1})), 'coeff');
 %find max of xcorr and index
-[cc{i,1},I{i,1}] = max(cross_cor{i,1});
+[cc{ii,1},I{ii,1}] = max(cross_cor{ii,1});
 %check if xcorr peak is high enough?
-if cc{i,1}>0.3
-    lagDiff(i,1) = lag{i,1}(I{i,1});
+if cc{ii,1}>0.3
+    lagDiff(ii,1) = lag{ii,1}(I{ii,1});
     %3 pt gaussian interpolation, to get a more exact velocity
-    peakOffset(i,1) = ((log(cross_cor{i,1}(I{i,1}-1)))-(log(cross_cor{i,1}(I{i,1}+1))))/(2*((log(cross_cor{i,1}(I{i,1}-1)))+(log(cross_cor{i,1}(I{i,1}+1)))-2*(log(cc{i,1}))));
+    peakOffset(ii,1) = ((log(cross_cor{ii,1}(I{ii,1}-1)))-(log(cross_cor{ii,1}(I{ii,1}+1))))/(2*((log(cross_cor{ii,1}(I{ii,1}-1)))+(log(cross_cor{ii,1}(I{ii,1}+1)))-2*(log(cc{ii,1}))));
     %find delta t
-    DT(i,1) = abs((lagDiff(i,1)+peakOffset(i,1))/FS(i,1));
+    DT(ii,1) = abs((lagDiff(ii,1)+peakOffset(ii,1))/FS(ii,1));
 else
     lagDiff = NaN;
     DT = NaN;
 end
 %disturbance velocity, [m/s]
-Uc(i,1) = (dx2)/DT(i,1);
+Uc(ii,1) = dx2(ii)/1000/DT(ii,1);
 
 % figure(4)
 % clf
 % plot(lag,cross_cor)
 end
 fig2 = figure(2);
-h2 = scatter(Uc(:,1),25.4*Y(:,1),'k*');
-hold on
+h2 = scatter(Uc(:,1),25.4*Y(:,1),25,Rex,'filled');
+cmap = colormap(jet);
+%cmap = [1  1  1 ; cmap]; %hide low re
+colormap(cmap)
+cb2 = colorbar();
+%caxis([1.4e6,inf])
+caxis([-inf,inf])
+cb2.Label.String = 'Re/x (m^{-1})';
 xlabel('Velocity (m/s)')
 ylabel('Height above floor (mm)')
+
+%% Print Voltages and Delta Ts
+writematrix(VOLT,'D:\FLDI_UMD\Voltages.xlsx');
+writematrix(DT,'D:\FLDI_UMD\Offsets.xlsx');
---
+++ PressureDataProcess.m
@@ -0,0 +1,69 @@
+%Process large Block of Pressure Data
+cd('D:\FLDI_UMD\PressureData')
+PressureFiles = dir('**/*_UMD')';
+
+for ii = 1:length(PressureFiles)
+    PressureData(ii).Fullname = [PressureFiles(ii).folder,'\',PressureFiles(ii).name];
+    PressureData(ii).RunNumber = ii;
+    if any(PressureData(ii).RunNumber == [12 21 27])
+        BadRun(ii) = 1;
+    else
+        BadRun(ii) = 0;
+    end
+ 
+    if PressureFiles(ii).bytes == 277
+        PressureData(ii).Useful = 0;
+    else
+        PressureData(ii).Useful = 1;
+    end
+end
+PressureData = PressureData(~BadRun);
+
+%handel some wierd matlab inport rules
+opts = detectImportOptions(PressureData(1).Fullname);
+opts.VariableNames = {'DValvePct' 'VValvePct' 'Inlet' 'TotalP' 'StaticP' 'SupplyP' 'VacuumP' 'FatPipeP' 'DriverA' 'DriverB' 'DriverC' 'DriverD' 'CameraTrigger'};
+        
+fs = 12.5e3;
+
+for ii = 20:length(PressureData)
+    %if statement to select runs with useful data
+    if PressureData(ii).Useful
+        
+        %check that the table looks right
+        Temp = preview(PressureData(ii).Fullname,opts)
+        fprintf('Press Enter if table looks ok')
+        pause
+        clc
+        %load data, use the camera trigger to normalize t and then plot
+        P = readtable(PressureData(ii).Fullname,opts);
+        t(:,1) = 0:1/fs:(height(P)-1)/fs;
+        Trigger = find(P.CameraTrigger > 0,1);
+        t = t - t(Trigger);
+        %plot
+            fig = figure('Visible','off','Position',[-1000 100 100 100],'WindowStyle','modal','WindowState','maximized');
+            plot(t, [P.FatPipeP P.DriverA])
+            xlim([-0.10,0.35])
+            legend(P.Properties.VariableNames([8 9]));
+            grid on
+            fig.Visible = 'on';
+            fprintf('need to enter 3 points. ')
+            fprintf('Preburst, start and stop of steady flow, only x vals matter')
+            [x,~] = ginput(3);
+            fig.WindowStyle = 'normal';
+            close(fig)
+            PressureData(ii).BurstP0= P.DriverA(find(t>=x(1),1)); 
+            PressureData(ii).RunP0 = mean([P.FatPipeP(find(t>=x(2),1):find(t>=x(3),1))]);
+            PressureData(ii).time_start = x(2);
+            PressureData(ii).time_end = x(3);
+            PressureData(ii).IsDataGood = input('Did that graph look okay? 1 for yes, 0 for no');
+    else
+        PressureData(ii).BurstP0= 60; 
+        PressureData(ii).RunP0 = 0.9*PressureData(ii).BurstP0;
+        PressureData(ii).time_start = 0.075;
+        PressureData(ii).time_end = 0.15;
+    end
+    clear t Temp Trigger x y P fig
+clc
+openvar('PressureData')
+end
+save('Pressures','PressureData','-v7.3')
