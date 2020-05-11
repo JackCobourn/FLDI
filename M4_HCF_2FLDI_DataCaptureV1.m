cb81664ff5b25bbd57f0b77e2a693867411b3ef5
Jack Cobourn <Jack@cobourn.com>
2020-05-11 07:13:00 -0500

    reduce chance of overwrites and change scope IP

--- M4_HCF_2FLDI_DataCaptureV1.m
+++ M4_HCF_2FLDI_DataCaptureV1.m
@@ -2,25 +2,25 @@
 %edited for HCF data capture by Jack Cobourn 20200319 based on original code by Andrew Ceruzi
 
 %% RUN CONDITIONS
-
+clear ISOdate date
 %date
 date = date(); %Comment out for testing on a different day
 ISOdate = yyyymmdd(datetime(date));
 
 %run numbers
 DailyRunNum = 1;
-CampainRunNum = 1;
+CampainRunNum = 7;
 PositionRunNumber = 1;
 
 %position
 tunnel_section = 1; %section of tunnel FLDI is probing
-x_dist = NaN;  % streamwise distance from nozzle/test section junction to first beam focus in inches (+-2mm)
-rail_pos = 25.4*7; % position of rail measured on the stands. should be laser position from back window.
-y_dist = 10*25.4;  % vertical distance from tunnel floor to laser points in inch (+-2mm)
+x_dist = 25.4*(6+6/32);  % streamwise distance model mount to forward laser position (+-2mm)
+rail_pos = 25.4*9.5; % position of rail measured on the stands. should be laser position from back window.
+y_dist = (10+59/64)*25.4;  % vertical distance from tunnel floor to laser points in inch (+-2mm)
 z_dist = 0;  % position of tunnel test section centerline relative to FLDI focus (cm)
 
 %FLDI Config
-dx = [285 316 683 714];
+dx = [357 395 816 854];
 dx1a = 0.00738*(dx(2)-dx(1))/1000; %FLDI beam separation
 dx1b =  0.00738*(dx(4)-dx(3))/1000;
 dx2 =  0.00738*(mean(dx([3,4]))-mean(dx([1,2])))/1000; %[m] 
@@ -37,7 +37,7 @@ bitRes = 8;
 num_diaphrams = 1;  % number of diaphrams in ludweig tube
 expected_burst_pressure = '~17Psia'; % tunnel high pressure in Psia
 
-model = 'NO MODEL'; %The model inserted in the tunnel during the run
+model = 'HCF'; %The model inserted in the tunnel during the run
 notes = {'chA is ch2, is upstream beam and downstream transducer';'set Both Chanels to 1MOhms'};
 
 
@@ -51,6 +51,12 @@ ACQ = Wavesurfer10.Acquisition; %simplify code with shorthands
 TRG = Wavesurfer10.Trigger;
 WVE = Wavesurfer10.Waveform;
 cd('..\')
+
+% %query(VSA,"VBS? 'return=app.Acquisition.Horizontal.SampleMode'")
+% if ~strcmpi(query(VSA,"VBS? 'return=app.Acquisition.Horizontal.SampleMode'"),'RealTime\n')
+%     fprintf(VSA, "VBS 'app.Acquisition.Horizontal.SampleMode = RealTime'");
+%     error('Incorrect Sample Mode')
+% end
 %% Record CHANNELS
 vmax(1) = input('Enter the ch A maximum voltage:');
 vmin(1) = input('Enter the ch A minimum voltage:');
@@ -62,17 +68,17 @@ vavg = (vmax+vmin)./2.0;
 vsep = vmax - vavg; % range of instrument for both channels
 
 
-ACoupling = query(VisaInterface,'C3:COUPLING?');
-BCoupling = query(VisaInterface,'C2:COUPLING?');
-AOffset = eval(query(VisaInterface,'C3:OFFSET?'));
-BOffset = eval(query(VisaInterface,'C2:OFFSET?'));
-AScale = eval(query(VisaInterface,'C3:VOLT_DIV?'))*10;
-BScale = eval(query(VisaInterface,'C2:VOLT_DIV?'))*10;
+ACoupling = query(VSA,'C3:COUPLING?');
+BCoupling = query(VSA,'C2:COUPLING?');
+AOffset = eval(query(VSA,'C3:OFFSET?'));
+BOffset = eval(query(VSA,'C2:OFFSET?'));
+AScale = eval(query(VSA,'C3:VOLT_DIV?'))*10;
+BScale = eval(query(VSA,'C2:VOLT_DIV?'))*10;
 
 %% GET TIMEBASE
 recordtime = get(ACQ,'TimeBase')*10; %seconds
 set(ACQ,'Delay',-get(ACQ,'TimeBase').*10./2) %set delay so that nothing from before trigger is saved
-numSamples = eval(query(VisaInterface,'MEMORY_SIZE?')); %%% Add to driver properly. also you always get 2 extra points
+numSamples = eval(query(VSA,'MEMORY_SIZE?')); %%% Add to driver properly. also you always get 2 extra points
 Fs = numSamples./recordtime; %Hz
 
 %% PAUSE FOR USER INPUT
@@ -202,17 +208,24 @@ disconnect(Wavesurfer10)
 
 %% SAVE
 disp('Saving data...')
-% %save the variables
-% save F:\FLDI_UMD\200204\UTSI_M4_2ptFLDI_y_2_25_inch_02.mat timeMs
 folderstring=sprintf('F:\\FLDI_HCF\\Data\\%d',ISOdate);
-whole = floor(y_dist); fraction = floor(100*mod(y_dist,1));
-savestring=sprintf('M4_2ptFLDI_HCF_y_%d_%d_inch_0%d',whole,fraction,PositionRunNumber);
-%mkdir(folderstring);
+savestring=sprintf('M4_2ptFLDI_HCF_CRun_%u_DRun_%u',CampainRunNum,DailyRunNum);
+if ~isfolder(folderstring)
+    mkdir(folderstring);
+end
 cd(folderstring)
+if isfile([savestring '.fig'])
+    error('FileExists')
+elseif isfile([savestring '.jpg'])
+    error('FileExists')
+elseif isfile([folderstring '\' savestring '.mat'])
+    error('FileExists')
+end
+savefig(fig3,[savestring '.fig']);
+saveas(fig3,savestring,'jpeg');
+clear fig3
 save([folderstring '\' savestring '.mat'],'-v7.3');
-fid = fopen([folderstring '\' savestring '.txt'],'w');
+fid = fopen([folderstring '\' savestring '.txt'],'a');
 fprintf(fid,'Uc = %.4f',Uc);
 fclose(fid);
-%savefig(fig3,[savestring '.fig']);
-%saveas(fig3,savestring,'jpeg')
 disp('DONE!')
