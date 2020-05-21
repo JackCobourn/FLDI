2c97b38abceedbdaf34fe6e0b0d861bde2bf6776
Jack Cobourn <Jack@cobourn.com>
2020-05-21 12:57:12 -0500

    JpegRead

---
+++ LBP2_Jpeg_read.m
@@ -0,0 +1,14 @@
+%% LBP2 Jpeg read
+% Jack Cobourn cw 20200519
+% Using: Newport LBP2-VIS2
+
+BP_File = 'D:\Jack\Desktop\20200515_center_0001.jpeg';
+BP = imread(BP_File);
+BP2 = reshape(BP,[],3);
+BP2Vals = unique(BP2,'rows');
+BP = imcrop=[161 0 width 655]
+BP(327:328,:,:) = repmat(0,[2,1207,3]);
+
+[centers,radii] = imfindcircles(BP,[2,10],'Sensitivity',0.9,'EdgeThreshold',.3);
+imshow(BP);
+viscircles(centers, radii,'Color','b');
